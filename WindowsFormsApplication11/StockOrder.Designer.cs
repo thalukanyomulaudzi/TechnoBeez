477631455c723453301e7b71e40813fee8a93ecc
thalukanyomulaudzi <thalu.tuks@gmail.com>
2018-07-21 13:30:37 +0200

    hgh

--- WindowsFormsApplication11/StockOrder.Designer.cs
+++ WindowsFormsApplication11/StockOrder.Designer.cs
@@ -32,9 +32,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StockOrder));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.txtUnits = new System.Windows.Forms.TextBox();
-            this.label15 = new System.Windows.Forms.Label();
-            this.txtinventoryName = new System.Windows.Forms.TextBox();
             this.button7 = new System.Windows.Forms.Button();
             this.panel17 = new System.Windows.Forms.Panel();
             this.panel14 = new System.Windows.Forms.Panel();
@@ -45,14 +42,16 @@
             this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.btnSelectSupplier = new System.Windows.Forms.Button();
-            this.btnSelectStock = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.employeeTypeBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
             this.genderBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.genderBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.employeeTypeBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.employeeTypeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel3.SuspendLayout();
             this.panel17.SuspendLayout();
             this.panel14.SuspendLayout();
@@ -66,49 +65,27 @@
             ((System.ComponentModel.ISupportInitialize)(this.genderBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.employeeTypeBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.employeeTypeBindingSource)).BeginInit();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(15, 31);
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(155, 20);
+            this.textBox1.Size = new System.Drawing.Size(164, 20);
             this.textBox1.TabIndex = 63;
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.groupBox4);
             this.panel3.Controls.Add(this.groupBox3);
             this.panel3.Controls.Add(this.groupBox2);
             this.panel3.Controls.Add(this.groupBox1);
-            this.panel3.Controls.Add(this.label15);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(0, 57);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(425, 245);
+            this.panel3.Size = new System.Drawing.Size(425, 258);
             this.panel3.TabIndex = 19;
-            // 
-            // txtUnits
-            // 
-            this.txtUnits.Location = new System.Drawing.Point(15, 19);
-            this.txtUnits.Name = "txtUnits";
-            this.txtUnits.Size = new System.Drawing.Size(219, 20);
-            this.txtUnits.TabIndex = 57;
-            // 
-            // label15
-            // 
-            this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(18, 128);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(65, 13);
-            this.label15.TabIndex = 56;
-            this.label15.Text = "Stock Type:";
-            // 
-            // txtinventoryName
-            // 
-            this.txtinventoryName.Location = new System.Drawing.Point(15, 19);
-            this.txtinventoryName.Name = "txtinventoryName";
-            this.txtinventoryName.Size = new System.Drawing.Size(219, 20);
-            this.txtinventoryName.TabIndex = 55;
             // 
             // button7
             // 
@@ -116,12 +93,12 @@
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
             this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button7.Location = new System.Drawing.Point(118, 15);
+            this.button7.Location = new System.Drawing.Point(118, 3);
             this.button7.Name = "button7";
             this.button7.Padding = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.button7.Size = new System.Drawing.Size(70, 34);
             this.button7.TabIndex = 9;
-            this.button7.Text = "Save";
+            this.button7.Text = "Order";
             this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button7.UseVisualStyleBackColor = true;
             // 
@@ -131,7 +108,7 @@
             this.panel17.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel17.Location = new System.Drawing.Point(225, 0);
             this.panel17.Name = "panel17";
-            this.panel17.Size = new System.Drawing.Size(200, 58);
+            this.panel17.Size = new System.Drawing.Size(200, 39);
             this.panel17.TabIndex = 8;
             // 
             // panel14
@@ -140,17 +117,17 @@
             this.panel14.Controls.Add(this.panel17);
             this.panel14.Controls.Add(this.panel15);
             this.panel14.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel14.Location = new System.Drawing.Point(0, 302);
+            this.panel14.Location = new System.Drawing.Point(0, 315);
             this.panel14.Margin = new System.Windows.Forms.Padding(0);
             this.panel14.Name = "panel14";
-            this.panel14.Size = new System.Drawing.Size(425, 59);
+            this.panel14.Size = new System.Drawing.Size(425, 40);
             this.panel14.TabIndex = 18;
             // 
             // panel15
             // 
             this.panel15.BackColor = System.Drawing.Color.Gainsboro;
             this.panel15.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel15.Location = new System.Drawing.Point(0, 58);
+            this.panel15.Location = new System.Drawing.Point(0, 39);
             this.panel15.Name = "panel15";
             this.panel15.Size = new System.Drawing.Size(425, 1);
             this.panel15.TabIndex = 0;
@@ -193,52 +170,30 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.btnSelectSupplier);
-            this.groupBox1.Controls.Add(this.txtinventoryName);
+            this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Location = new System.Drawing.Point(21, 17);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(374, 65);
+            this.groupBox1.Size = new System.Drawing.Size(263, 57);
             this.groupBox1.TabIndex = 64;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Supplier";
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.btnSelectStock);
-            this.groupBox2.Controls.Add(this.txtUnits);
-            this.groupBox2.Location = new System.Drawing.Point(21, 94);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Location = new System.Drawing.Point(21, 80);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(374, 64);
+            this.groupBox2.Size = new System.Drawing.Size(263, 53);
             this.groupBox2.TabIndex = 65;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Stock";
             // 
-            // btnSelectSupplier
-            // 
-            this.btnSelectSupplier.Location = new System.Drawing.Point(240, 19);
-            this.btnSelectSupplier.Name = "btnSelectSupplier";
-            this.btnSelectSupplier.Size = new System.Drawing.Size(128, 23);
-            this.btnSelectSupplier.TabIndex = 56;
-            this.btnSelectSupplier.Text = "Select Supplier";
-            this.btnSelectSupplier.UseVisualStyleBackColor = true;
-            this.btnSelectSupplier.Click += new System.EventHandler(this.btnSelectSupplier_Click);
-            // 
-            // btnSelectStock
-            // 
-            this.btnSelectStock.Location = new System.Drawing.Point(240, 16);
-            this.btnSelectStock.Name = "btnSelectStock";
-            this.btnSelectStock.Size = new System.Drawing.Size(128, 23);
-            this.btnSelectStock.TabIndex = 58;
-            this.btnSelectStock.Text = "Select Stock";
-            this.btnSelectStock.UseVisualStyleBackColor = true;
-            this.btnSelectStock.Click += new System.EventHandler(this.btnSelectStock_Click);
-            // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.textBox1);
-            this.groupBox3.Location = new System.Drawing.Point(21, 164);
+            this.groupBox3.Location = new System.Drawing.Point(21, 198);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(200, 68);
+            this.groupBox3.Size = new System.Drawing.Size(206, 47);
             this.groupBox3.TabIndex = 66;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Quantity";
@@ -251,27 +206,57 @@
             // 
             this.genderBindingSource1.DataMember = "Gender";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(224, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(6, 19);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(224, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox2);
+            this.groupBox4.Location = new System.Drawing.Point(21, 139);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(263, 53);
+            this.groupBox4.TabIndex = 67;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Stock Description";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 20);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(224, 20);
+            this.textBox2.TabIndex = 0;
+            // 
             // StockOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(425, 361);
+            this.ClientSize = new System.Drawing.Size(425, 355);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel14);
             this.Controls.Add(this.panel2);
             this.Name = "StockOrder";
             this.Text = "StockOrder";
             this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.panel17.ResumeLayout(false);
             this.panel14.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
             this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.employeeTypeBindingSource2)).EndInit();
@@ -279,6 +264,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.genderBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.employeeTypeBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.employeeTypeBindingSource)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -292,9 +279,6 @@
         private System.Windows.Forms.BindingSource employeeTypeBindingSource;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.TextBox txtUnits;
-        private System.Windows.Forms.Label label15;
-        private System.Windows.Forms.TextBox txtinventoryName;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Panel panel17;
         private System.Windows.Forms.Panel panel14;
@@ -305,8 +289,10 @@
         private System.Windows.Forms.ErrorProvider errorProvider;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button btnSelectStock;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button btnSelectSupplier;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
